a7dc010d39164f10390536ad43ec257ff2f1a43c
drozdu02 <olivier.drozdowski02@gmail.com>
2026-09-13 15:54:38 +0200

    feat: Adding http-only for refreshing access tokens

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Res, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service.js';
 import { RegisterDto } from './dto/register.dto.js';
 import { RegisterResponseDto } from './dto/register-response.dto.js';
@@ -6,6 +6,7 @@ import { LoginDto } from './dto/login.dto.js';
 import { LoginResponseDto } from './dto/login-response.dto.js';
 import { JwtAuthGuard } from './guards/jwt-auth.guard.js';
 import { CurrentUser } from './decorators/current-user.decorator.js';
+import type { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
@@ -19,10 +20,25 @@ export class AuthController {
   }
 
   @Post('login')
-  loginUser(
-    @Body() loginDto: LoginDto
+  async loginUser(
+    @Body() loginDto: LoginDto,
+    @Res({ passthrough: true }) response: Response,
   ): Promise<LoginResponseDto> {
-    return this.authService.login(loginDto);
+    const result = await this.authService.login(loginDto);
+
+    response.cookie(
+      'refresh_token',
+      result.refreshToken,
+      {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'lax',
+        maxAge: 1000 * 60 * 60 * 24 * 30
+      },
+    );
+    return {
+      accessToken: result.accessToken
+    }
   }
 
   @UseGuards(JwtAuthGuard)
--- src/auth/dto/login-response.dto.ts
+++ src/auth/dto/login-response.dto.ts
@@ -1,7 +1,12 @@
-import { IsNotEmpty, IsString } from "class-validator";
+import { IsNotEmpty, IsOptional, IsString } from "class-validator";
 
 export class LoginResponseDto {
     @IsString()
     @IsNotEmpty()
     accessToken: string;
+    
+    @IsString()
+    @IsNotEmpty()
+    @IsOptional()
+    refreshToken?: string;
 }
