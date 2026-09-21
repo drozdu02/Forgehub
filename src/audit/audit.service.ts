b27be029b93121cd1089f9b092ddc9b2db1f9ac3
drozdu02 <olivier.drozdowski02@gmail.com>
2026-09-21 19:03:38 +0200

    feat: Adding creating audit log

--- src/audit/audit.service.ts
+++ src/audit/audit.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { AuditLog } from './entities/audit-log.entity.js';
+import { Repository } from 'typeorm';
+import { CreateAuditLogInputDto } from './dto/create-audit-log-input.dto.js';
 
 @Injectable()
-export class AuditService {}
+export class AuditService {
+    constructor(
+        @InjectRepository(AuditLog)
+        private readonly auditLogRepository: Repository<AuditLog>,
+
+    ) {}
+
+    async create(
+        createAuditLogInputDto: CreateAuditLogInputDto
+    ): Promise<AuditLog> {
+        const auditLog = this.auditLogRepository.create({
+            organization: {
+                id: createAuditLogInputDto.organizationId
+            },
+            actor: createAuditLogInputDto.actorUserId ? { id: createAuditLogInputDto.actorUserId } : null,
+            action: createAuditLogInputDto.action,
+            entityType: createAuditLogInputDto.entityType,
+            entityId: createAuditLogInputDto.entityId,
+            metadata: createAuditLogInputDto.metadata ?? null,
+        });
+        return this.auditLogRepository.save(auditLog);
+    }
+}
