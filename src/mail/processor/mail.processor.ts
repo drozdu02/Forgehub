66a2739a8bcab232969a60241aefa7fea91d76d6
drozdu02 <olivier.drozdowski02@gmail.com>
2026-09-21 16:43:35 +0200

    feat: Integrating mail processor with aws provider

--- src/mail/processor/mail.processor.ts
+++ src/mail/processor/mail.processor.ts
@@ -1,24 +1,33 @@
 import { Processor, WorkerHost } from "@nestjs/bullmq";
+import { Inject } from "@nestjs/common";
 import { Job } from "bullmq";
+import { EMAIL_PROVIDER } from "../constants/email-provider.constant.js";
+import type { EmailProvider } from "../providers/email.provider.js";
 
 @Processor('mail')
 export class MailProcessor extends WorkerHost {
+
+    constructor(
+        @Inject(EMAIL_PROVIDER)
+        private readonly emailProvider: EmailProvider
+    ) {
+        super();
+    }
 
     async process(
         job: Job
     ): Promise<void> {
         if (job.name === 'send-verification-email') {
-            await this.sendVerificationEmail(job);
+            await this.emailProvider.sendVerificationEmail({
+                to: job.data.email,
+                code: job.data.code,
+            });
+            return;
         }
-        return;
-    }
-
-    private async sendVerificationEmail(
-        job: Job
-    ): Promise<void> {
-        console.log(
-            'Sending verification email',
-            job.data
+        throw new Error(
+            `Unknown mail job: ${job.name}`
         );
     }
+
+    
 }
