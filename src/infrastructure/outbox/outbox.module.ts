9dc71294ed10ad04445a530956142cbe39a5048e
drozdu02 <olivier.drozdowski02@gmail.com>
2026-09-23 12:05:53 +0200

    feat: Creating outbox processor for events

--- src/infrastructure/outbox/outbox.module.ts
+++ src/infrastructure/outbox/outbox.module.ts
@@ -2,13 +2,14 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { OutboxEvent } from './entities/outbox-event.entity.js';
 import OutboxService from './outbox.service.js';
+import { OutboxProcessor } from './outbox.processor.js';
 @Module({
   imports: [
       TypeOrmModule.forFeature([
         OutboxEvent
       ])
     ],
-  providers: [OutboxService],
+  providers: [OutboxService, OutboxProcessor],
   exports: [OutboxService],
 })
 export class TasksModule {}
